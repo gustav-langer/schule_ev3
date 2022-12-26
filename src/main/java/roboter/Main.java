6ba6eb9682b5768ab591efcc87c81f293b04c2cb
BuildTools <unconfigured@null.spigotmc.org>
2022-12-26 17:34:04 +0100

    start

--- src/main/java/roboter/Main.java
+++ src/main/java/roboter/Main.java
@@ -1,4 +1,22 @@
 package roboter;
 
+import ev3dev.actuators.lego.motors.*;
+import lejos.hardware.port.*;
+
+/*
+Was der Roboter können muss:
+-Motoren müssen benutzt werden
+-Er muss sich bewegen können (Motoren benutzen)
+-Etwas auf dem Bildschirm zeigen
+
+
+Ideen:
+-Zu Musik tanzen
+ */
 public class Main {
+    public static void main(String[] args) {
+        NXTRegulatedMotor leftLeg = new NXTRegulatedMotor(MotorPort.B);
+        NXTRegulatedMotor rightLeg = new NXTRegulatedMotor(MotorPort.C);
+        System.out.println("Done");
+    }
 }
