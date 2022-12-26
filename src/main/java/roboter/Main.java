42485767979dd56622a12b67f9103c392e38ea6e
BuildTools <unconfigured@null.spigotmc.org>
2022-12-26 17:51:32 +0100

    Added a "Hello World" program

--- src/main/java/roboter/Main.java
+++ src/main/java/roboter/Main.java
@@ -1,6 +1,8 @@
 package roboter;
 
+import ev3dev.actuators.*;
 import ev3dev.actuators.lego.motors.*;
+import lejos.hardware.lcd.*;
 import lejos.hardware.port.*;
 
 /*
@@ -13,10 +15,13 @@ Was der Roboter können muss:
 Ideen:
 -Zu Musik tanzen
  */
+
 public class Main {
     public static void main(String[] args) {
         NXTRegulatedMotor leftLeg = new NXTRegulatedMotor(MotorPort.B);
         NXTRegulatedMotor rightLeg = new NXTRegulatedMotor(MotorPort.C);
-        System.out.println("Done");
+        GraphicsLCD screen = LCD.getInstance();
+        String data = "Hello World!";
+        screen.drawChars(data.toCharArray(),0,data.length(),0,0,0);
     }
 }
