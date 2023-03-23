2445c62c0a27123c4de6776a704761667663f62b
gustav-langer <ich.leake.nicht@meine.email.adresse>
2023-03-23 21:44:47 +0100

    Sehr sehr vieles

--- src/main/java/roboter/Main.java
+++ src/main/java/roboter/Main.java
@@ -1,29 +1,14 @@
 package roboter;
 
-import ev3dev.actuators.LCD;
-import ev3dev.actuators.lego.motors.NXTRegulatedMotor;
-import ev3dev.robotics.tts.Espeak;
-import ev3dev.sensors.EV3Key;
-import ev3dev.sensors.ev3.EV3TouchSensor;
 import ev3dev.utils.JarResource;
-import ev3dev.utils.Shell;
-import ev3dev.utils.Sysfs;
-import lejos.hardware.Key;
-import lejos.hardware.lcd.GraphicsLCD;
-import lejos.hardware.port.MotorPort;
-import lejos.hardware.port.SensorPort;
-import lejos.robotics.RegulatedMotor;
-import lejos.robotics.Touch;
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
+import lejos.utility.Delay;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.List;
+
+import static roboter.DanceMoves.ONE_STEP;
 
 /*
 Was der Roboter können muss:
@@ -36,20 +21,50 @@ Ideen:
 -Zu Musik tanzen
  */
 
+//todo: Annotations hinkriegen
+
 public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         Robot robot = new Robot(true);
-        demo(robot);
+        LOGGER.info("Roboter erzeugt!");
+        robot.beep();
+        robot.any.waitForPressAndRelease();
+        dance(robot, args.length == 0 ? 120 : Integer.parseInt(args[0]));
     }
 
-    static void demo(@NotNull Robot robot) {
-        Speed baseSpeed = Speed.ev3(45);
-        LOGGER.info("Roboter erzeugt!");
+    static void dance(Robot robot, int bpm) {
+        var msPerBeat = 60000 / bpm;
+        Speed baseSpeed = Speed.ev3(bpm / 4);// baseSpeed ~= 1 Umdrehung/Takt
+        List<DanceMove> dance = List.of(
+                ONE_STEP,
+                ONE_STEP,
+                ONE_STEP,
+                ONE_STEP,
+                ONE_STEP);
         try {
-            robot.getLcd().drawImage(JarResource.loadImage(JarResource.JAVA_DUKE_IMAGE_NAME), 35, 10, 0);
+            robot.getLcd().drawImage(JarResource.loadImage("java_logo_2.png"), 35, 10, 0);
             robot.getLcd().refresh();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        robot.startPlayingFile("song.wav");
+        Delay.msDelay(Math.round(msPerBeat * 16));
+        /*robot.move(baseSpeed.mult(2), 8);
+        robot.move(baseSpeed.mult(2).neg(), 8);*/
+        runDance(robot, baseSpeed, dance);
+        robot.stopAudio();
+    }
+
+    static void runDance(Robot robot, Speed speed, List<DanceMove> dance) {
+        dance.forEach((move) -> move.execute(robot, speed));
+    }
+
+    static void demo(Robot robot) {
+        Speed baseSpeed = Speed.ev3(45);
+        try {
+            robot.drawImageAndRefresh(JarResource.loadImage("ev3_logo.png"), 35, 10, 0);
         } catch (IOException e) {
             LOGGER.info("Couldn't load image");
         }
@@ -57,7 +72,7 @@ public class Main {
         robot.say("Hello");
         robot.move(baseSpeed.neg(), 7);
         robot.turn(baseSpeed.offset(5), -90);
-        robot.startPlayingFile("song2.wav").whenComplete((v, e) -> {
+        robot.startPlayingFile("song.wav").whenComplete((v, e) -> {
             if ((e) != null) {
                 throw new RuntimeException(e);
             } else System.exit(0);
@@ -66,260 +81,6 @@ public class Main {
         robot.any.waitForPressAndRelease();
         robot.stopAudio();
         LOGGER.info("stopped");
-    }
-}
-
-class Robot {
-    static final boolean USE_INIT_ASYNC = false;
-    private static final Logger LOGGER = LoggerFactory.getLogger(Robot.class);
-    Key any = new EV3Key(EV3Key.BUTTON_ALL);
-    private RegulatedMotor rightMotor;
-    private RegulatedMotor leftMotor;
-    private Touch leftSensor;
-    private Touch rightSensor;
-    private GraphicsLCD lcd;
-    private Espeak espeak = new Espeak();
-    private boolean isInitiallyCalibrated = false; // Wird beim ersten Kalibrieren auf true gesetzt
-    private int leftCalibratedAngle;
-    private int rightCalibratedAngle;
-
-    Robot(boolean doImmediateCalibration) {
-        Runtime.getRuntime().addShutdownHook(new Thread(this::stop)); // Roboter stoppen, wenn das Programm endet
-        if (USE_INIT_ASYNC) {
-            try {
-                initAsync(doImmediateCalibration);
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.err.flush();
-                throw new RuntimeException();
-            }
-        } else {
-            init(doImmediateCalibration);
-        }
-    }
-
-    private void initAsync(boolean doImmediateCalibration) throws ExecutionException, InterruptedException {
-        CompletableFuture<RegulatedMotor> leftFuture = CompletableFuture.<RegulatedMotor>supplyAsync(() -> new NXTRegulatedMotor(MotorPort.C))
-                .exceptionally((e) -> {
-            LOGGER.info("Motor C not found");
-                    // Immer wieder versuchen, den Motor zu finden, da der Roboter manchmal beim ersten oder zweiten Aufruf zu langsam ist.
-            while (true) {
-                try {
-                    return new NXTRegulatedMotor(MotorPort.C);
-                } catch (RuntimeException f) {
-                    LOGGER.info("Motor C not found");
-                }
-            }
-        });
-        LOGGER.info("Starting left (C), sleeping");
-        //Thread.sleep(10000);
-        //LOGGER.info("Sleep done");
-        CompletableFuture<RegulatedMotor> rightFuture = CompletableFuture.<RegulatedMotor>supplyAsync(() -> new NXTRegulatedMotor(MotorPort.B))
-                .exceptionally((e) -> {
-            LOGGER.info("Motor B not found");
-            while (true) {
-                try {
-                    return new NXTRegulatedMotor(MotorPort.B);
-                } catch (RuntimeException f) {
-                    LOGGER.info("Motor B not found");
-                }
-            }
-        });
-        LOGGER.info("Starting right (B), sleeping");
-        //Thread.sleep(10000);
-        //LOGGER.info("Sleep done");
-        CompletableFuture<Touch> leftSensorFuture = CompletableFuture.<Touch>supplyAsync(() -> new EV3TouchSensor(SensorPort.S2))
-                .exceptionally((e) -> {
-            LOGGER.info("Sensor S2 not found");
-            // Wenn das passiert, wurde der Sensor nicht erkannt. Mit Sysfs.writeString kann man den Sensormodus manuell setzen
-            Sysfs.writeString("/sys/class/lego-port/port1/set_device", "lego-nxt-touch");
-            return new EV3TouchSensor(SensorPort.S2);
-        });
-        CompletableFuture<Touch> rightSensorFuture = CompletableFuture.<Touch>supplyAsync(() -> new EV3TouchSensor(SensorPort.S1))
-                .exceptionally((e) -> {
-            LOGGER.info("Sensor S1 not found");
-            Sysfs.writeString("/sys/class/lego-port/port0/set_device", "lego-nxt-touch");
-            return new EV3TouchSensor(SensorPort.S1);
-        });
-        CompletableFuture<GraphicsLCD> lcdFuture = CompletableFuture.supplyAsync(LCD::getInstance);
-        try {
-            leftMotor = leftFuture.get(1, TimeUnit.MINUTES);
-            rightMotor = rightFuture.get(1, TimeUnit.MINUTES);
-        } catch (TimeoutException e) {
-            LOGGER.error("Motors timed out");
-            throw new RuntimeException(e);
-        }
-        leftSensor = leftSensorFuture.get();
-        rightSensor = rightSensorFuture.get();
-        CompletableFuture<Void> calibrateFuture = CompletableFuture.runAsync(() -> {
-            if (doImmediateCalibration) calibrate(true);
-        });
-        lcd = lcdFuture.get();
-        espeak = new Espeak();
-        calibrateFuture.get(); // Warten, bis das Kalibrieren abgeschlossen ist
-    }
-
-    private void init(boolean doImmediateCalibration) {
-        leftMotor = new NXTRegulatedMotor(MotorPort.C);
-        rightMotor = new NXTRegulatedMotor(MotorPort.B);
-        try {
-            leftSensor = new EV3TouchSensor(SensorPort.S2);
-            rightSensor = new EV3TouchSensor(SensorPort.S1);
-        } catch (RuntimeException e) {
-            Sysfs.writeString("/sys/class/lego-port/port0/set_device", "lego-nxt-touch");
-            Sysfs.writeString("/sys/class/lego-port/port1/set_device", "lego-nxt-touch");
-            leftSensor = new EV3TouchSensor(SensorPort.S2);
-            rightSensor = new EV3TouchSensor(SensorPort.S1);
-        }
-        lcd = LCD.getInstance();
-        espeak = new Espeak();
-        if (doImmediateCalibration) {
-            calibrate(true);
-        }
-    }
-
-    void calibrate() {
-        calibrate(false);
-    }
-
-    void calibrate(boolean forceCalibrate) {
-        if (forceCalibrate || !isInitiallyCalibrated
-                || (leftCalibratedAngle != leftMotor.getTachoCount() % 360)
-                || (rightCalibratedAngle != rightMotor.getTachoCount() % 360)) {
-            int calibrateSpeed = 270;
-            leftMotor.setSpeed(calibrateSpeed);
-            if (leftSensor.isPressed()) leftMotor.rotate(180);
-            leftMotor.forward();
-            while (!leftSensor.isPressed()) Thread.onSpinWait();
-            leftMotor.stop();
-            rightMotor.setSpeed(calibrateSpeed);
-            if (rightSensor.isPressed()) rightMotor.rotate(180);
-            rightMotor.forward();
-            while (!rightSensor.isPressed()) Thread.onSpinWait();
-            rightMotor.stop();
-            leftMotor.rotate(180);
-            leftCalibratedAngle = leftMotor.getTachoCount() % 360;
-            rightCalibratedAngle = rightMotor.getTachoCount() % 360;
-            isInitiallyCalibrated = true;
-        }
-    }
-
-    void forward(@NotNull Speed s) {
-        leftMotor.setSpeed(s.getJava());
-        rightMotor.setSpeed(s.getJava());
-        leftMotor.forward();
-        rightMotor.forward();
-    }
-
-    void backward(@NotNull Speed s) {
-        leftMotor.setSpeed(s.getJava());
-        rightMotor.setSpeed(s.getJava());
-        leftMotor.backward();
-        rightMotor.backward();
-    }
-
-    void stop() {
-        leftMotor.stop(true);
-        rightMotor.stop();
-    }
-
-    void move(Speed s, int steps) {
-        move(s, steps, false);
-    }
-
-    void move(@NotNull Speed s, int steps, boolean immediateReturn) {
-        leftMotor.setSpeed(s.getJava());
-        rightMotor.setSpeed(s.getJava());
-        if (s.isPositive()) {
-            leftMotor.rotate(steps * 360, true);
-            rightMotor.rotate(steps * 360, immediateReturn);
-        } else {
-            leftMotor.rotate(steps * -360, true);
-            rightMotor.rotate(steps * -360, immediateReturn);
-        }
-    }
-
-    void turn(Speed s, int degrees) {
-        // Die Motoren müssen sich um degrees * rotationFactor Grad drehen, damit sich der Roboter um degrees dreht
-        //Herausgefunden durch Trial-and-Error
-        final int rotationFactor = 60;
-        if (degrees < 0) {
-            calibrate();
-            leftMotor.rotate(180);
-            rightMotor.rotate(36 + 180);
-            leftMotor.setSpeed(s.getJava());
-            leftMotor.rotate(rotationFactor * degrees);
-        } else {
-            calibrate();
-            leftMotor.rotate(36);
-            rightMotor.setSpeed(s.getJava());
-            rightMotor.rotate(rotationFactor * degrees);
-        }
-    }
-
-    GraphicsLCD getLcd() {
-        return this.lcd;
-    }
-
-    void say(String message) {
-        espeak.setMessage(message);
-        espeak.say();
-    }
-
-    CompletableFuture<Void> startPlayingFile(String path) {
-        return CompletableFuture.runAsync(() -> playFile(path));
-    }
-
-    void playFile(String path) {
-        Shell.execute("aplay " + path);
-    }
-
-    void stopAudio() {
-        Shell.execute("killall aplay");
-    }
-}
-
-class Speed {
-    // Funktionen wie setSpeed() messen Geschwindigkeit in Grad/Sekunde. Der grafische Programmeditor
-    // misst Geschwindigkeit in Umdrehungen/Minute, deshalb diese Klasse zum einfachen Umrechnen
-    private final int javaSpeed; //Geschwindigkeit in Grad/Sekunde
-
-    private Speed(int javaSpeed) {
-        this.javaSpeed = javaSpeed;
-    }
-
-    @Contract(value = "_ -> new", pure = true)
-    static @NotNull Speed java(int value) {
-        return new Speed(value);
-    }
-
-    @Contract(value = "_ -> new", pure = true)
-    static @NotNull Speed ev3(int value) {
-        return new Speed(value * 6);
-    }
-
-    int getJava() {
-        return javaSpeed;
-    }
-
-    int getEv3() {
-        return javaSpeed / 6;
-    }
-
-    boolean isPositive() {
-        return javaSpeed > 0;
-    }
-
-    Speed offset(int offset) {
-        return Speed.java(this.javaSpeed + offset);
-    }
-
-    Speed mult(int factor) {
-        return Speed.java(this.javaSpeed * factor);
-    }
-
-    Speed neg() {
-        return mult(-1);
     }
 }
 
