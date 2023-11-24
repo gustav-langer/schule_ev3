df75d532cc62c3d2fe3350230c8b7f001d5364d3
gustav-langer <ich.leake.nicht@meine.email.adresse>
2023-11-24 15:48:47 +0100

    Zeilenumbrüche, MOVE_RIGHT_MOTOR_20 und MOVE_LEFT_MOTOR_20 auf die richtigen Funktionen umgestellt

--- src/main/java/roboter/DanceMoves.java
+++ src/main/java/roboter/DanceMoves.java
@@ -17,31 +17,28 @@ class DanceMoves {
     static final DanceMove SINGLE_BACKWARD_STEP_4 = SINGLE_BACKWARD_STEP_2.repeat(2);
 
     static final DanceMove TWO_FORWARD_STEPS_4 = steps(2);
-
     static final DanceMove TWO_BACKWARD_STEPS_4 = steps(-2);
 
     static final DanceMove FOUR_STEPS_FORWARD_4 = (robot, speed) -> robot.move(speed.mult(2), 4);
-
     static final DanceMove FOUR_STEPS_BACKWARD_4 = (robot, speed) -> robot.move(speed.mult(2), -4);
 
     static final DanceMove MOVE_ARMS_4 = (robot, speed) ->
             robot.rotateArmsMotor(speed.mult(2), RotateAmount.rotations(2));
-
     static final DanceMove MOVE_ARMS_ALT_4 = (robot, speed) ->
             robot.rotateArmsMotor(speed.mult(2), RotateAmount.rotations(-2));
 
     static final DanceMove ARMS_ON_0 = (robot, speed) -> robot.startArms(speed.mult(2));
-
     static final DanceMove ARMS_OFF_0 = (robot, speed) -> robot.stopArms();
+
     static final DanceMove MOVE_RIGHT_MOTOR_20 = (robot, speed) -> {
-        robot.rotateSingleMotor(robot.getLeftMotor(), speed, RotateAmount.rotations(-0.15F - 0.5F));
-        robot.rotateSingleMotor(robot.getRightMotor(), speed.mult(2), RotateAmount.rotations(8 - 0.6F));
-        robot.rotateSingleMotor(robot.getLeftMotor(), speed.negate(), RotateAmount.rotations(-0.15F - 0.5F));
+        robot.rotateLeftMotor(speed, RotateAmount.rotations(-0.15F - 0.5F));
+        robot.rotateRightMotor(speed.mult(2), RotateAmount.rotations(8 - 0.6F));
+        robot.rotateLeftMotor(speed.negate(), RotateAmount.rotations(-0.15F - 0.5F));
     };
     static final DanceMove MOVE_LEFT_MOTOR_20 = (robot, speed) -> {
-        robot.rotateSingleMotor(robot.getRightMotor(), speed, RotateAmount.rotations(-0.15F - 0.5F));
-        robot.rotateSingleMotor(robot.getLeftMotor(), speed.mult(2), RotateAmount.rotations(8 - 0.6F));
-        robot.rotateSingleMotor(robot.getRightMotor(), speed.negate(), RotateAmount.rotations(-0.15F - 0.5F));
+        robot.rotateRightMotor(speed, RotateAmount.rotations(-0.15F - 0.5F));
+        robot.rotateLeftMotor(speed.mult(2), RotateAmount.rotations(8 - 0.6F));
+        robot.rotateRightMotor(speed.negate(), RotateAmount.rotations(-0.15F - 0.5F));
     };
 
     private DanceMoves() {
